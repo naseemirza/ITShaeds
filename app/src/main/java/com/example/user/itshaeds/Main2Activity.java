8f4223f15712a7efb7a337a9365a5e69e760fa5d
Naseem Mirza <naseemirza786@gmail.com>
2018-08-21 18:17:54 +0530

    landscapprobsolutions

--- app/src/main/java/com/example/user/itshaeds/Main2Activity.java
+++ app/src/main/java/com/example/user/itshaeds/Main2Activity.java
@@ -275,21 +275,22 @@ public class Main2Activity extends AppCompatActivity
         return true;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a Parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a Parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_search) {
 //
-//        //noinspection SimplifiableIfStatement
-////        if (id == R.id.action_search) {
-////
-////            return true;
-////        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
